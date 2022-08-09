66be9d425d82809194562d776d23f00d3a2bb340
Jameela Cherry <83510929+jameelacherry@users.noreply.github.com>
2022-08-09 15:55:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+setTimeout(()=>{
+    document.body.style.backgroundColor='pink';
+},1000)
+setTimeout(()=>{
+    document.body.style.backgroundColor='red';
+},2000)
+
+setTimeout(()=>{
+    document.body.style.backgroundColor='yellow';
+
+  
+},3000)
